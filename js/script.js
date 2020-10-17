76490355947f4497f8f2c50fb0a6497e2db55d91
matveygrishaev <matveygrishaev@gmail.com>
2020-10-17 23:21:24 +0300

    lesson07 save2

--- js/script.js
+++ js/script.js
@@ -16,73 +16,61 @@ start();
 
 
 let appData = {
+    budget: money,
+    budgetDay: 0,
+    budgetMonth: 0,
+    expensesMonth: 0,
     income: {},
     addIncome: [],
-    expenses: {},
+    expenses: [],
+    getExpensesMonth: function() {
+
+        let sum = 0;
+        let sum2 = 0;
+
+        for (let i = 0; i < 2; i++) {
+            
+            appData.expenses[i] = prompt('Введите обязательную статью расходов?', 'Аренда');
+
+            do {
+                sum2 = prompt('Во сколько это обойдется?', 3000);
+            }
+            while (!isNumber(sum2));
+            sum = Number(sum) + Number(sum2);
+
+        }
+        console.log(appData.expenses);
+        return sum;
+    },
+    getAccumulatedMonth: function() {
+        return money - expensesAmount;
+    },
     addExpenses: [],
     deposit: false,
     mission: 50000,
     period: 3,
+    getTargetMonth: function() {
+        return appData.mission / accumulatedMonth;
+    },
     asking: function() {
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
             appData.addExpenses = addExpenses.toLowerCase().split(', ');
             appData.deposit = confirm('Есть ли у вас депозит в банке');
+    },
+    getStatusIncome: function() {
+            if (getTargetMonth() > 0) {
+                console.log('Цель будет достигнута')
+            } else {
+                console.log('Цель не будет достигнута');
+            }
     }
 };
 
+appData.asking()
 
-let showTypeOf = function(item) {
-    console.log(typeof item);
-};
-
-showTypeOf(money);
-showTypeOf(appData.income);
-showTypeOf(appData.deposit);
-
-let expenses = [];
-
-// Расходы за месяц getExpensesMonth
-let getExpensesMonth = function() {
-    // Переменная sum, которая суммирует расходы
-    let sum = 0;
-    let sum2 = 0;
-
-    // С помощью цикла будем задавать вопрос про расходы и складывать сумму расходов
-    for (let i = 0; i < 2; i++) {
-        
-        expenses[i] = prompt('Введите обязательную статью расходов?', 'Аренда');
-
-        do {
-            sum2 = prompt('Во сколько это обойдется?', 3000);
-        }
-        while (!isNumber(sum2));
-        sum = Number(sum) + Number(sum2)
-
-    }
-    console.log(expenses);
-    return sum;
-};
-
-let expensesAmount = getExpensesMonth();
+let expensesAmount = appData.getExpensesMonth();
 
 console.log('Расходы за месяц: ', expensesAmount);
 
-// Сбережения за месяц
-let getAccumulatedMonth = function() {
-    return money - expensesAmount;
-};
-
-// Значение переменной приравнивается результату функции 
-let accumulatedMonth = getAccumulatedMonth();
+let accumulatedMonth = appData.getAccumulatedMonth();
 console.log('Сбережения за месяц: ', accumulatedMonth);
-
-// Рассчет периода достижения цели
-let getTargetMonth = function() {
-    return appData.mission / accumulatedMonth;
-};
-console.log('Период достижения цели: ', getTargetMonth());
-
-if (getTargetMonth() > 0) {
-    console.log('Цель будет достигнута');
-    } else {console.log('Цель не будет достигнута');
-}
